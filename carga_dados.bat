51432b75cf889070e442e3afa7c173bc69710218
Rodrigo Fermiano <rc.fermiano@hotmail.com>
2023-06-23 12:22:54 -0300

    collection do postman para testes

--- carga_dados.bat
+++
@@ -1,6 +0,0 @@
-cd .\app\src\modelo.Infrastructure\
-dotnet ef migrations add Initial -s ..\modelo.API\ -c DBContext
-cd .\app\src\modelo.Infrastructure\
-dotnet ef database update -s ..\modelo.API\ -c DBContext
-
-set /p DUMMY=Hit ENTER to continue...
